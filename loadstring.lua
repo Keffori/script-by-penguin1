37ac4cb97f569dbd05a9fb6a43a970571d787e8d
Keffori <90979483+Keffori@users.noreply.github.com>
2023-02-25 16:15:26 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,5 @@
+_G.Uses = 1 -- how many times the crucifix will work
+_G.Range = 30 -- how far away the thing has to be
+_G.OnClick = true -- makes it so if you press q and left click while looking at something, a crucifix will apear in your inventory that works on what you were looking at
+_G.Variant = "Normal" -- Devil Eletric and Normal are the only variants for now
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Keffori/script-by-penguin1/main/main.lua'))() 
